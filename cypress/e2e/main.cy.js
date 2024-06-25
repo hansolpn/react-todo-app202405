56193a1b91d7079d8b4cea68ae0970ce1439f928
Hansol Na <142369934+hansolpn@users.noreply.github.com>
2024-06-25 11:11:50 +0900

    메인 화면 테스트 추가

--- cypress/e2e/main.cy.js
+++ cypress/e2e/main.cy.js
@@ -8,4 +8,26 @@ describe('메인화면', () => {
     cy.visit('http://localhost:3000/todo');
     cy.get('h1').eq(0).contains('로그인');
   });
+
+  it('로그인 되어 있다면 todo 화면으로 이동', () => {
+    cy.intercept('http://localhost:8181/api/todos**', {
+      statusCode: 200,
+      body: { todos: [] },
+    }).as('getTodos');
+    cy.intercept(
+      'http://localhost:8181/api/auth/load-profile',
+      {
+        statusCode: 201,
+      },
+    );
+    localStorage.setItem('ACCESS_TOKEN', '1');
+    localStorage.setItem('LOGIN_USERNAME', 'test user');
+    localStorage.setItem('USER_ROLE', 'COMMON');
+
+    cy.visit('http://localhost:3000/todo');
+
+    cy.wait('@getTodos');
+
+    cy.get('div').should('have.class', 'TodoTemplate');
+  });
 });
